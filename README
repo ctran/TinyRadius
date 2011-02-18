979ef7f8d9a878a35645d1b0a75410a7a3cdb890
Cuong Tran <cuong.tran@gmail.com>
2011-02-18 11:45:32 -0800

    first import from sourceforge.net

---
+++ README
@@ -0,0 +1,17 @@
+TinyRadius README
+
+TinyRadius is a simple, small and fast Java Radius library capable of
+sending and receiving Radius packets of all types. It is released
+under the terms of the LGPL.
+
+For documentation, please have a look at doc/tinyradius.html
+and doc/apidoc. Every method has got JavaDoc comments, and every
+package has got a package.html document detailing the package
+usage.
+
+Please do not hesitate to contact me if you have got questions or
+suggestions.
+
+Matthias Wuttke
+post@matthias-wuttke.de
+http://tinyradius.sourceforge.net/
---
+++ src/org/tinyradius/test/TestDictionary.java
@@ -0,0 +1,35 @@
+/**
+ * $Id: TestDictionary.java,v 1.1 2005/09/06 16:38:41 wuttke Exp $
+ * Created on 06.09.2005
+ * @author mw
+ * @version $Revision: 1.1 $
+ */
+package org.tinyradius.test;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.tinyradius.attribute.IpAttribute;
+import org.tinyradius.dictionary.Dictionary;
+import org.tinyradius.dictionary.DictionaryParser;
+import org.tinyradius.packet.AccessRequest;
+
+/**
+ * Shows how to use TinyRadius with a custom dictionary
+ * loaded from a dictionary file.
+ * Requires a file "test.dictionary" in the current directory.
+ */
+public class TestDictionary {
+
+	public static void main(String[] args) 
+	throws Exception {
+		InputStream source = new FileInputStream("test.dictionary");
+		Dictionary dictionary = DictionaryParser.parseDictionary(source);
+		AccessRequest ar = new AccessRequest("UserName", "UserPassword");
+		ar.setDictionary(dictionary);
+		ar.addAttribute("WISPr-Location-ID", "LocationID");
+		ar.addAttribute(new IpAttribute(8, 1234567));
+		System.out.println(ar);
+	}
+	
+}
