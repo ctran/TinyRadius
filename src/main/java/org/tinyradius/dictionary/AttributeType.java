e84c69c6717ecdeb65bc54549151819341725963
Cuong Tran <ctran@users.noreply.github.com>
2019-11-15 21:55:34 +0000

    Remove copyright to use the project-wide license (LGPL)
    The special copyright text was left here as mistake.  This should use the same license indicated as the top level and confirmed by the original author.

--- src/main/java/org/tinyradius/dictionary/AttributeType.java
+++ src/main/java/org/tinyradius/dictionary/AttributeType.java
@@ -1,10 +1,5 @@
 /**
  * $Id: AttributeType.java,v 1.3 2005/09/06 18:06:33 wuttke Exp $
- * Copyright by teuto.net Netzdienste GmbH 2005. All rights reserved.
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation. Commercial licenses also available.
- * See the accompanying file LICENSE for details.
  * 
  * @author Matthias Wuttke
  * @version $Revision: 1.3 $
