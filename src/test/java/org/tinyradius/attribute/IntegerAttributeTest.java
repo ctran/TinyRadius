2d449256ad8a94f23699782e3dfca659a21c30a5
Rick Frey <gribnut@gmail.com>
2021-12-27 16:35:27 -0600

    Migrate to Junit 5

--- src/test/java/org/tinyradius/attribute/IntegerAttributeTest.java
+++ src/test/java/org/tinyradius/attribute/IntegerAttributeTest.java
@@ -1,20 +1,21 @@
 package org.tinyradius.attribute;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
 
 public class IntegerAttributeTest {
 
 	@Test
 	public void test() {
 		final IntegerAttribute intAttr = new IntegerAttribute(27, 0);
-		final long bigValue = 0xffffffffL; // big value with highest bit set
-		System.err.println((int)bigValue);
-		System.err.println(bigValue);
+		final long bigValue = 0xffffffffL; // big value with the highest bit set
 		final String bigValueSt = Long.toString(bigValue);
 		intAttr.setAttributeValue(bigValueSt);
-		assertEquals(bigValueSt, intAttr.getAttributeValue());
+		//System.err.println(intAttr.getAttributeTypeObject().getName());
+		Assertions.assertEquals(IntegerAttribute.class, intAttr.getAttributeTypeObject().getAttributeClass());
+		Assertions.assertEquals("Session-Timeout", intAttr.getAttributeTypeObject().getName());
+		Assertions.assertEquals(bigValueSt, intAttr.getAttributeValue());
 	}
 
 }
